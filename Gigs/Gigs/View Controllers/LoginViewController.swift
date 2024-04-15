0c79c28228f9307cd46dd2337ef888768de8989c
Carlos "Mannie" Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2024-04-15 19:41:34 -0400

    Wired up storyboard with view controllers, app automatically shows the login view then goes back to table view after sign in.

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    enum LoginType: String  {
+        case signIn = "Sign In"
+        case signUp = "Sign Up"
+    }
+    
     @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signinButton: UIButton!
     
+    var authController: AuthController?
     
+    var loginType: LoginType = .signUp
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +41,59 @@ class LoginViewController: UIViewController {
     }
     */
     
-    @IBAction func setLoginType(_ sender: Any) {
+    @IBAction func setLoginType(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            loginType = .signUp
+            signinButton.setTitle(LoginType.signUp.rawValue, for: .normal)
+        case 1:
+            loginType = .signIn
+            signinButton.setTitle(LoginType.signIn.rawValue, for: .normal)
+        default:
+            break
+        }
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
+        guard let username = usernameTextField.text,
+                !username.isEmpty,
+                let password = passwordTextField.text,
+                !password.isEmpty
+        else { return }
+        
+        let user = User(username: username, password: password)
+        if loginType == .signUp {
+            authController?.signUp(with: user, completion: { error in
+                if let error = error {
+                    NSLog("There was an error during sign up: \(error)")
+                    return
+                }
+                
+                let alert = UIAlertController(title: "Sign Up Successful", message: "Now please Log In", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(okAction)
+                
+                DispatchQueue.main.async {
+                    self.present(alert, animated: true) {
+                        self.loginType =  .signIn
+                        self.signinButton.setTitle(LoginType.signIn.rawValue, for: .normal)
+                        self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                    }
+                    
+                }
+            })
+        } else {
+            authController?.signIn(with: user, completion: { error in
+                if let error = error {
+                    NSLog("There was an error during sign in: \(error)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true)
+                }
+            })
+        }
     }
     
 }
