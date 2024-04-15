7cd4fe33946ba2d4eaff3fd47427fe6fd715bf38
Carlos "Mannie" Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2024-04-15 17:34:31 -0400

    Wired up outlets and actions for the loginviewcontroller

---
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Carlos E. Alvarez-Berrios on 4/15/24.
+//  Copyright © 2024 Emani Computers and Support, LLC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signinButton: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func setLoginType(_ sender: Any) {
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+}
